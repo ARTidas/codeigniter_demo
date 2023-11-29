79db1f1fdd638518775bf2c3c7da13f440cd6d96
Veres Zoltán <artidas@artidas.hu>
2023-11-29 18:31:27 +0100

    Initialized first static page.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+use App\Controllers\Helloworld;
+
+$routes->get('helloworld', [Helloworld::class, 'getIndex']);
